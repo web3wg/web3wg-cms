9892b45762a81b85b20fba361db3f696263c83b8
Willian Ferreira <williangf@hotmail.com>
2023-03-14 17:39:34 -0300

    Host fix

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,31 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": [
+            "'self'",
+            "https:",
+            "https://strapi.web3wg.org",
+            "http://strapi.web3wg.org",
+            "http://localhost:1337",
+            "http://127.0.0.1:1337",
+            "http://0.0.0.0:1337",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
